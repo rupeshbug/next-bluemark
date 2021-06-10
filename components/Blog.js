92b7fb705c77567f7127b401b953d2eaca535828
Rupesh <chaulagainrupesh1@gmail.com>
2021-06-10 19:40:30 +0545

    progress

--- components/Blog.js
+++ components/Blog.js
@@ -9,7 +9,7 @@ const blogs = [
     img: <Image src='/blogImg1.jpg' height={200} width={400} />,
     author: 'John Doe',
     date: '2020-01-20',
-    tag: 'culture',
+    category: 'culture',
     title: 'Aligning Your Employees To Company Goals Using OKR',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
   },
@@ -17,7 +17,7 @@ const blogs = [
     img: <Image src='/blogImg2.jpg' height={200} width={400} />,
     author: 'John Doe',
     date: '2020-01-20',
-    tag: 'startup',
+    category: 'startup',
     title: 'How to Keep Investors Happy While Building A Business?',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
   },
@@ -25,7 +25,7 @@ const blogs = [
     img: <Image src='/blogImg1.jpg' height={200} width={400} />,
     author: 'John Doe',
     date: '2020-01-20',
-    tag: 'startup',
+    category: 'startup',
     title: 'Aligning Your Employees To Company Goals Using OKR',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
   }
@@ -44,22 +44,22 @@ const Blog = () => {
             <div className={styles.cardMedia}>
               { blog.img }
             </div>
-            <Link href='/'><a className={styles.tag}>{ blog.tag }</a></Link>
-            <div className={styles.authorInfo}>
-              <div className={styles.date}>
-                <i className="far fa-clock"></i>
-                <p>{ blog.date }</p>
+            <Link href='/'><a className={styles.category}>{ blog.category }</a></Link>
+              <div className={styles.authorInfo}>
+                <div className={styles.date}>
+                  <i className="far fa-clock"></i>
+                  <p>{ blog.date }</p>
+                </div>
+                <div>
+                  <i className="fas fa-user"></i>
+                  <p>{ blog.author }</p>
+                </div>
               </div>
-              <div>
-                <i className="fas fa-user"></i>
-                <p>{ blog.author }</p>
+              <div className={styles.blogText}>
+                <h4>{ blog.title }</h4>
+                <p>{ blog.description }</p>
               </div>
-            </div>
-            <div className={styles.blogText}>
-              <h4>{ blog.title }</h4>
-              <p>{ blog.description }</p>
-            </div>
-            <Link href="/"><a>Read More</a></Link>
+              <Link href="/"><a>Read More</a></Link>
           </div>
         ))}
       </div>
