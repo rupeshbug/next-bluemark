7a6d6e31d561bd6a448fa500092c081b6b5b1206
Rupesh <chaulagainrupesh1@gmail.com>
2021-06-23 13:29:54 +0545

    animation added to Profile

--- components/Profile.js
+++ components/Profile.js
@@ -1,20 +1,24 @@
 import styles from '../styles/Profile.module.css'
 import CountUp from 'react-countup'
+import { useInView } from 'react-intersection-observer'
 
 const Profile = () => {
+
+  const [ref, inView] = useInView();
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.cardBody}>
           <h2>Get <span className={styles.highlighter}>insights</span> that help your business grow.</h2>
           <p>Discover the benefits of data analysis ad make better decision regarding Discover the benefits of data analysis ad make better decision regarding revenue, customer, experience, and overall efficiency.</p>
-          <div className={styles.cardData}>
+          <div className={styles.cardData} ref={ref}>
             <div>
-              <CountUp start={0} end={100} duration={2}></CountUp> +
+              <CountUp end={inView ? 100 : 0} duration={3}></CountUp> +
               <p>companies</p>
             </div>
             <div>
-              <CountUp start={0} end={250} duration={2}></CountUp> +
+              <CountUp end={inView ? 250 : 0} duration={3}></CountUp> +
               <p>templates</p>
             </div>
           </div>
